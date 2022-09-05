059166595f6ed31562053c054d68d8989b8263be
Francesco Mariani <francesco.mariani@flowing.it>
2022-09-05 16:16:09 +0200

    Initial Commit

---
+++ claranet-base-ios/claranet_base_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  claranet_base_iosApp.swift
+//  claranet-base-ios
+//
+//  Created by Francesco on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct claranet_base_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
